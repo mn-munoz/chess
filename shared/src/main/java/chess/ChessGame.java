ab9c91c62fbe69683a0cd7c40c77d82c6eb09482
Merleth <merlethmunozra@gmail.com>
2024-06-28 16:21:34 -0600

    worked on ChessGame.java a little bit

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -10,12 +10,16 @@ import java.util.Collection;
  */
 public class ChessGame {
 
-    public ChessGame() {
+    boolean isWhitesTurn = true;
+    TeamColor currentTeam;
 
+    public ChessGame() {
+        this.currentTeam = TeamColor.WHITE;
     }
 
     /**
      * @return Which team's turn it is
+     *
      */
     public TeamColor getTeamTurn() {
         throw new RuntimeException("Not implemented");
@@ -27,7 +31,7 @@ public class ChessGame {
      * @param team the team whose turn it is
      */
     public void setTeamTurn(TeamColor team) {
-        throw new RuntimeException("Not implemented");
+        this.currentTeam = team;
     }
 
     /**
