f7d84b4ae086af260fd517d14b5c09ab6cbdfbac
Merleth <merlethmunozra@gmail.com>
2024-07-17 10:10:17 -0600

    Added server object to initialize server on port 8080

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,8 +1,21 @@
 import chess.*;
+import spark.Spark;
 
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Server: " + piece);
+
+        int port = 8080;
+        Spark.port(port);
+        Spark.staticFileLocation("/web");
+
+        createRoutes();
+        Spark.awaitInitialization();
+        System.out.println("Listening on port " + port);
+
+    }
+    private static void createRoutes() {
+        Spark.get("/hello", (req, res) -> "Hello BYU!");
     }
 }
