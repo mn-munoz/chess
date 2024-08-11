8398614072c1168133a0136b5a4d6aa7502c88db
Merleth <merlethmunozra@gmail.com>
2024-08-10 18:39:36 -0600

    Started stablishing websocket code in server

---
+++ server/src/main/java/server/websocket/WebSocketHandler.java
@@ -0,0 +1,24 @@
+package server.websocket;
+
+import com.google.gson.Gson;
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
+import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+import websocket.commands.UserGameCommand;
+
+import java.io.IOException;
+
+@WebSocket
+public class WebSocketHandler {
+
+    @OnWebSocketMessage
+    public void onMessage(Session session, String message) throws IOException {
+        UserGameCommand command = new Gson().fromJson(message, UserGameCommand.class);
+        switch (command.getCommandType()) {
+            case CONNECT -> enter(command.visitorName(), session);
+            case MAKE_MOVE -> exit(command.visitorName());
+            case LEAVE -> ;
+            case RESIGN -> ;
+        }
+    }
+}
